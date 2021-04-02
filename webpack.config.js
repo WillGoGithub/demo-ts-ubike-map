026adc44931e892d0e724500a93e632baefa7d01
+W <willohyayo@gmail.com>
2021-04-02 14:53:21 +0800

    env done

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
-import { resolve as _resolve } from 'path';
+const path = require('path');
 
-export const entry = './src/index.ts';
-export const module = {
+module.exports = {
+  entry: './src/index.ts',
+  module: {
     rules: [
-        {
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-};
-export const resolve = {
+  },
+  resolve: {
     extensions: ['.ts', '.js'],
-};
-export const output = {
+  },
+  output: {
     filename: 'bundle.js',
-    path: _resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
